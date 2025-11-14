070bac4943ae625318697d58a1548b7440f63c7f
lavanyapadaga5 <104807917+lavanyapadaga5@users.noreply.github.com>
2025-11-14 15:48:51 -0500

    Create urls.py

---
+++ Ecommerce/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('users.urls')),
+
+    # JWT token endpoints
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # app routes
+    path('api/users/', include('users.urls')),
+    path('api/products/', include('products.urls')),
+    path('api/billing/', include('billing.urls')),
+]
