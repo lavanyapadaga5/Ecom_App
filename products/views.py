c5ad9d582ff156475ff50bdccc92e305c0fd6d2d
lavanyapadaga5 <104807917+lavanyapadaga5@users.noreply.github.com>
2025-11-14 15:40:55 -0500

    Add files via upload

---
+++ products/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics, permissions
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
