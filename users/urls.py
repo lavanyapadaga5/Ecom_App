f0ac7810bb387a57feda2fa50a48412b575bd52b
lavanyapadaga5 <104807917+lavanyapadaga5@users.noreply.github.com>
2025-11-17 16:08:57 -0500

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
-from .views import ProductListCreateView, ProductDetailView
+from django.urls import path, include
+from .views import RegisterView, UpdateProfile, UserDetailView, LoginView, UserProfile, LogoutView
+from rest_framework.routers import DefaultRouter
+
 
 urlpatterns = [
-    path('', ProductListCreateView.as_view(), name='product-list-create'),
-    path('<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('me/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('profile/', UserProfile.as_view(), name='profile'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', UserProfile.as_view(), name='profile'),
+    path('profile/update/', UpdateProfile.as_view(), name='profile-update'),
+
+
 ]
