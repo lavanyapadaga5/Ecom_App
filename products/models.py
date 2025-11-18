169f03a724068840e14441e89d1e2229ab5c7d20
lavanyapadaga5 <104807917+lavanyapadaga5@users.noreply.github.com>
2025-11-18 15:44:55 -0500

    Update models.py

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
@@ -9,3 +10,10 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product.name}"
