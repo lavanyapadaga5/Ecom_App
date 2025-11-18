f303421f647241db07788eb59edab92a47b0f9aa
lavanyapadaga5 <104807917+lavanyapadaga5@users.noreply.github.com>
2025-11-18 15:40:17 -0500

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import RegisterView, UpdateProfile, UserDetailView, LoginView, UserProfile, LogoutView
+from .views import RegisterView, UpdateProfile, UserDetailView, LoginView, UserProfile, LogoutView, ChangePasswordView, AddAddressView, AddressListView
 from rest_framework.routers import DefaultRouter
 
 
@@ -11,6 +11,8 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', UserProfile.as_view(), name='profile'),
     path('profile/update/', UpdateProfile.as_view(), name='profile-update'),
-
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('add-address/', AddAddressView.as_view(), name='add-address'),
+    path('addresses/', AddressListView.as_view(), name='addresses'),
 
 ]
