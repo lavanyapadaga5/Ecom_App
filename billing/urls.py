8adefb99425e077f6aac1637f448dd3bf7fc9b60
lavanyapadaga5 <104807917+lavanyapadaga5@users.noreply.github.com>
2025-11-18 15:48:56 -0500

    Update urls.py

--- billing/urls.py
+++ billing/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
 from .views import (
     OrderCreateView, OrderListView,
-    CartView, AddToCartView,
-    UpdateCartItemView, RemoveCartItemView
+    CartView, BulkAddToCartView,
+    UpdateCartItemView, RemoveCartItemView, OrderHistoryView, PlaceOrderView, CancelOrderView
 )
 
 urlpatterns = [
     path('cart/', CartView.as_view(), name='cart'),
-    path('cart/add/', AddToCartView.as_view(), name='add-to-cart'),
+    path('cart/add/', BulkAddToCartView.as_view(), name='add-to-cart'),
     path('cart/update/', UpdateCartItemView.as_view(), name='update-cart-item'),
     path('cart/remove/', RemoveCartItemView.as_view(), name='remove-cart-item'),
     path('orders/', OrderListView.as_view(), name='order-list'),
     path('orders/create/', OrderCreateView.as_view(), name='order-create'),
+    path("orders/history/", OrderHistoryView.as_view(), name="order-history"),
+    path("place-order/", PlaceOrderView.as_view(), name="place_order"),
+    path("cancel-order/<int:order_id>/", CancelOrderView.as_view(), name="cancel_order"),
 ]
