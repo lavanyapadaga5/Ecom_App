4c5e709637791dc85b550ec0b0217247bc1af301
lavanyapadaga5 <104807917+lavanyapadaga5@users.noreply.github.com>
2025-11-18 15:42:43 -0500

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ProductListCreateView, ProductDetailView
+from .views import ProductListCreateView, ProductDetailView, AddToWishlistView, RemoveFromWishlistView, WishlistView
 
 urlpatterns = [
     path('', ProductListCreateView.as_view(), name='product-list-create'),
     path('<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('wishlist/add/', AddToWishlistView.as_view(), name='wishlist-add'),
+    path('wishlist/remove/', RemoveFromWishlistView.as_view(), name='wishlist-remove'),
+    path('wishlist/', WishlistView.as_view(), name='wishlist'),
 ]
