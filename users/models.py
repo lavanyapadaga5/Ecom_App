ed46f9ae1459b3ff6a46748728c4a912419949cf
lavanyapadaga5 <104807917+lavanyapadaga5@users.noreply.github.com>
2025-11-18 15:41:13 -0500

    Update models.py

--- users/models.py
+++ users/models.py
@@ -7,3 +7,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+    house_no = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    pincode = models.CharField(max_length=6)
+
+    def __str__(self):
+        return f"{self.full_name} - {self.city}"
